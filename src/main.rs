192dad1bcb0907dd2e52eea4dc9a2a355bf2946f
Nico Orru <nigu.orru@gmail.com>
2024-07-31 19:03:21 +0100

    Added sanitisation and templating with Handlebars

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use chrono::DateTime;
 use chrono::NaiveDateTime;
-use chrono::{DateTime, Utc};
 use clap::Parser;
 use glob::*;
 use log::*;
 use log4rs::append::console::ConsoleAppender;
-use std::collections::{BTreeMap, HashMap};
-use std::ffi::OsStr;
-use std::fmt::Display;
+use serde_json::value::*;
+use sha1::{Digest, Sha1};
+use std::collections::BTreeMap;
+use std::fmt::Write;
 use std::fs;
 use std::io;
 use std::path::Path;
@@ -17,18 +18,23 @@ use std::time::UNIX_EPOCH;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-	#[arg(short, long, default_value = "**/*.ARW")]
-	glob: String,
+	#[arg()]
+	globs: Vec<String>,
 
 	#[arg(short, long, default_value = "%Y%m%d_%H%M%S")]
 	date_time_format: String,
 
-	#[arg(short, long, default_value = "%p/%n.%e")]
-	name_pattern: String,
-}
+	#[arg(short, long, default_value = "{{sys.Path}}/{{sys.Name}}_{{sys.Sha1}}.{{sys.Ext}}")]
+	out: String,
 
-struct App {
-	args: Args,
+	#[arg(short, long, default_value_t = false)]
+	verbose: bool,
+
+	#[arg(short, long, default_value = "[^\\w\\+\\-]+")]
+	sanitize: String,
+
+	#[arg(short, long, default_value = "_")]
+	replacement: String,
 }
 
 #[derive(Clone, Debug)]
@@ -64,7 +70,7 @@ impl ExifAttr {
 
 	fn from_opt_str_datetime(from: Option<&str>) -> Self {
 		match from {
-			Some(word) => match chrono::NaiveDateTime::parse_from_str(word, "%Y:%m:%d %H:%M:%S") {
+			Some(word) => match NaiveDateTime::parse_from_str(word, "%Y:%m:%d %H:%M:%S") {
 				Ok(dt) => ExifAttr::Timestamp(dt),
 				Err(e) => {
 					warn!("Unable to parse '{}' as date: {:?}", word, e);
@@ -123,42 +129,130 @@ impl ExifAttr {
 	}
 }
 
+struct ExifAttrFormatter {
+	date_time_format: String,
+	sanitize_pattern: regex::Regex,
+	sanitize_replacement: String,
+}
+
+impl ExifAttrFormatter {
+	fn new(
+		date_time_format: String,
+		sanitize_pattern: &str,
+		sanitize_replacement: String,
+	) -> Result<Self, regex::Error> {
+		Ok(ExifAttrFormatter {
+			date_time_format,
+			sanitize_pattern: regex::Regex::new(sanitize_pattern)?,
+			sanitize_replacement,
+		})
+	}
+
+	fn fmt<W>(&self, value: &ExifAttr, f: &mut W) -> std::fmt::Result
+	where W: Write {
+		match value {
+			// write!(f, "{}", strings)
+			ExifAttr::Text(ref text) => f.write_str(text),
+			ExifAttr::Path(ref path) => f.write_str(path.to_str().unwrap_or("")),
+			ExifAttr::Timestamp(ref timestamp) => f.write_str(&timestamp.format(&self.date_time_format).to_string()),
+			ExifAttr::Integer(ref value) => write!(f, "{}", value),
+			ExifAttr::Fraction(ref num, ref den) => write!(f, "{}_{}", num, den),
+			ExifAttr::Real(ref value) => write!(f, "{}", value),
+			ExifAttr::Nothing => Ok(()),
+		}
+	}
+
+	fn sanitize(&self, value: &String) -> String {
+		self.sanitize_pattern.replace_all(value, &self.sanitize_replacement).to_string()
+	}
+
+	pub fn as_string(&self, value: &ExifAttr) -> String {
+		let mut value_as_string = String::new();
+		if let Err(e) = self.fmt(&value, &mut value_as_string) {
+			error!("Cannot convert {:?} to string: {}", value, e)
+		}
+		match value {
+			ExifAttr::Path(_) => value_as_string,
+			_ => self.sanitize(&value_as_string),
+		}
+	}
+}
+
+struct App {
+	args: Args,
+	fmt: ExifAttrFormatter,
+}
+
 impl App {
-	pub fn find_matches(&self) -> Result<Vec<PathBuf>, PatternError> {
+	fn new(args: Args) -> Result<Self, regex::Error> {
+		let fmt = ExifAttrFormatter::new(args.date_time_format.clone(), &args.sanitize, args.replacement.clone())?;
+
+		Ok(App { args, fmt })
+	}
+
+	fn find_matches(&self, pattern: &str) -> Result<Vec<PathBuf>, PatternError> {
 		let mut out = Vec::new();
-		for iter in glob::glob(&self.args.glob)? {
+		for iter in glob::glob(pattern)? {
 			match iter {
 				Ok(path) => out.push(path),
-				Err(e) => error!("{:?}", e),
+				Err(e) => error!("Invalid glob pattern {}: {}", pattern, e),
 			}
 		}
 		return Ok(out);
 	}
 
-	pub fn extract_props(&self, path: &PathBuf) -> BTreeMap<String, ExifAttr> {
+	fn extract_props(&self, path: &PathBuf) -> BTreeMap<String, ExifAttr> {
 		let mut out = BTreeMap::new();
-		out.insert("fs.Ext".to_owned(), ExifAttr::from_opt_str(path.extension().map(OsStr::to_str).flatten()));
-		out.insert("fs.Name".to_owned(), ExifAttr::from_opt_str(path.file_stem().map(OsStr::to_str).flatten()));
-		out.insert("fs.FullName".to_owned(), ExifAttr::from_opt_str(path.file_name().map(OsStr::to_str).flatten()));
-		out.insert("fs.Path".to_owned(), ExifAttr::from_opt_path(path.parent()));
-
+		out.insert(
+			"sys.Ext".to_owned(),
+			ExifAttr::from_opt_path(path.extension().map(PathBuf::from).as_ref().map(PathBuf::as_path)),
+		);
+		out.insert(
+			"sys.Name".to_owned(),
+			ExifAttr::from_opt_path(path.file_stem().map(PathBuf::from).as_ref().map(PathBuf::as_path)),
+		);
+		out.insert(
+			"sys.FullName".to_owned(),
+			ExifAttr::from_opt_path(path.file_name().map(PathBuf::from).as_ref().map(PathBuf::as_path)),
+		);
+		out.insert("sys.Path".to_owned(), ExifAttr::from_opt_path(path.parent()));
+		let mut r_path = PathBuf::new();
+		for (i, component) in path.components().enumerate() {
+			out.insert(
+				format!("sys.Path{}", i),
+				ExifAttr::from_opt_path(Some(PathBuf::from(component.as_os_str()).as_path())),
+			);
+			r_path.push(component);
+			out.insert(format!("fs.RPath{}", i), ExifAttr::from_opt_path(Some(r_path.as_path())));
+		}
 		match fs::metadata(path) {
 			Ok(metadata) => {
-				out.insert("fs.DateTimeCreated".to_owned(), ExifAttr::from_opt_filetime(metadata.created().ok()));
-				out.insert("fs.DateTimeModified".to_owned(), ExifAttr::from_opt_filetime(metadata.modified().ok()));
-				out.insert("fs.DateTimeAccessed".to_owned(), ExifAttr::from_opt_filetime(metadata.accessed().ok()));
-				out.insert("fs.FileSize".to_owned(), ExifAttr::Integer(metadata.len() as i64));
+				out.insert("sys.DateTimeCreated".to_owned(), ExifAttr::from_opt_filetime(metadata.created().ok()));
+				out.insert("sys.DateTimeModified".to_owned(), ExifAttr::from_opt_filetime(metadata.modified().ok()));
+				out.insert("sys.DateTimeAccessed".to_owned(), ExifAttr::from_opt_filetime(metadata.accessed().ok()));
+				out.insert("sys.Size".to_owned(), ExifAttr::Integer(metadata.len() as i64));
 			}
 			Err(err) => error!("Unable to read metadata for {:?}: {:?}", path, err),
 		}
+
+		if let Ok(mut file) = fs::File::open(&path) {
+			let mut hasher = Sha1::new();
+			match io::copy(&mut file, &mut hasher) {
+				Ok(_) => {
+					out.insert("sys.Sha1".to_owned(), ExifAttr::Text(hex::encode(hasher.finalize())));
+				}
+				Err(e) => error!("Unable to compute hash for {:?}: {:?}", &path, e),
+			}
+		}
+
 		let exif_file = fs::File::open(path);
 		match exif_file {
 			Ok(file) => {
-				let mut bufreader = io::BufReader::new(&file);
-				let exifreader = exif::Reader::new();
-				if let Ok(exif) = exifreader.read_from_container(&mut bufreader) {
+				let mut buf_reader = io::BufReader::new(&file);
+				let exif_reader = exif::Reader::new();
+				if let Ok(exif) = exif_reader.read_from_container(&mut buf_reader) {
 					for f in exif.fields() {
-						info!(
+						debug!(
 							"{:30} {:50} {:10} {:.50}",
 							f.tag,
 							f.tag.description().unwrap_or(""),
@@ -196,6 +290,34 @@ impl App {
 		}
 		out
 	}
+
+	fn run(&self) {
+		for glob in &self.args.globs {
+			info!("Matching pattern '{}'", glob);
+			let paths = self.find_matches(glob).expect("Error extracting source files");
+
+			let reg = handlebars::Handlebars::new();
+
+			for src_path in &paths {
+				let props = self.extract_props(src_path);
+				let mut data = serde_json::value::Map::new();
+				for (key, value) in &props {
+					let value_as_string = self.fmt.as_string(&value);
+					if self.args.verbose {
+						info!("{:50} '{}'", key, value_as_string);
+					}
+					data.insert(key.to_owned(), Value::String(value_as_string));
+				}
+				match reg.render_template(&self.args.out, &data) {
+					Ok(dest) => {
+						let dest_path = PathBuf::from(dest);
+						info!("{:?} -> {:?}", src_path, dest_path);
+					}
+					Err(e) => error!("Invalid pattern or data {}: {}", &self.args.out, e),
+				}
+			}
+		}
+	}
 }
 
 fn main() {
@@ -207,14 +329,6 @@ fn main() {
 
 	init_config(log_config.expect("Invalid log configuration")).expect("Unable to initialize log4rs");
 
-	let app = App { args: Args::parse() };
-
-	info!("Matching>: {}", &app.args.glob);
-
-	let paths = app.find_matches().expect("Error extracting source files");
-
-	for path in &paths {
-		info!("{:?}", &path);
-		info!("{:?}", app.extract_props(path));
-	}
+	let app = App::new(Args::parse()).expect("Invalid arguments");
+	app.run();
 }
